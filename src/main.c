4fd7ae2a2027750eacb98066044ca22394c7e77f
rkerman <rkerman@student.42.fr>
2025-04-24 23:59:48 +0200

    file processing function

--- src/main.c
+++ src/main.c
@@ -6,13 +6,12 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 19:03:47 by rkerman           #+#    #+#             */
-/*   Updated: 2025/04/24 15:57:19 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/04/24 23:59:17 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdio.h>
-
 
 int	countwords(char *str)
 {
@@ -40,7 +39,8 @@ int	is_num(char *s)
 	int	i;
 
 	i = 0;
-	while (s[i] == ',' || s[i] == ' ' || s[i] == '\n' || (s[i] >= '0' && s[i] <= '9')
+	while (s[i] == ',' || s[i] == ' '
+		|| s[i] == '\n' || (s[i] >= '0' && s[i] <= '9')
 		|| s[i] == '-' || (s[i] >= 'a' && s[i] <= 'f') || s[i] == 'x'
 		|| (s[i] >= 'A' && s[i] <= 'F') || s[i] == 'X')
 		i++;
@@ -51,39 +51,39 @@ int	is_num(char *s)
 	}
 	return (1);
 }
-	
-int	main(int argc, char **argv)
-{
-//	void	*mlx;
-//	void	*mlx_win;
-	int		fd;
-	char	*line;
-	int		size;
-	int		c;
 
-	c = 0;
-	if ((argc - 1) == 1)
+int	file_processing(char *path, int **grid)
+{
+	int	fd;
+	char	*line;
+	int	size;
+	int	c;
+
+	fd = open(path, O_RDONLY);
+	if (fd != -1)
 	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd != -1)
+		line = get_next_line(fd);
+		size = countwords(line);
+		while (line)
 		{
+			if (!size)
+				size = countwords(line);
+			if (size != countwords(line) || !is_num(line))
+				return (free(line), 0);
+			c++;
+			free(line);
 			line = get_next_line(fd);
-			size = countwords(line);
-			while (line)
-			{
-				if (!size)
-					size = countwords(line);
-				if (size != countwords(line) /*|| !is_num(line)*/)
-					return (write(2, "Error map\n", 10), 0);
-				c++;
-				printf("%d\n%d\n", size, c);
-				free(line);
-				line = get_next_line(fd);
-			}
 		}
 		close(fd);
+		return (1);
 	}
-//	mlx = mlx_init();
-//	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-//	mlx_loop(mlx);
+}
+
+int	main(int argc, char **argv)
+{
+	int	*g;
+
+	if ((argc - 1) == 1 && file_processing(argv[1], &g))
+	{
+	}
 }
