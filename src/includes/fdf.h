a81c846a3b920a20ffb590f18b0d1830dfc35ed2
Rayan Kerman <rkerman@c3r3p6.42perpignan.fr>
2025-04-27 16:13:40 +0200

    parser making..

--- src/includes/fdf.h
+++ src/includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 19:04:02 by rkerman           #+#    #+#             */
-/*   Updated: 2025/04/24 15:03:06 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/04/27 15:09:01 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,16 @@
 #include <stdint.h>
 #include <fcntl.h>
 
+typedef struct t_value
+{
+	int	size;
+	int	width;
+	int	height;
+}	t_val;
+
+
 char	*get_next_line(int fd);
-int	ft_chr_c(char *s, int c);
+int		ft_chr_c(char *s, int c);
 char	*ft_strjoin(char *s, char *b);
 int		ft_strlen(char *str);
 void	*ft_calloc(size_t nmemb, size_t size);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rkerman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 19:03:47 by rkerman           #+#    #+#             */
-/*   Updated: 2025/04/24 23:59:17 by rkerman          ###   ########.fr       */
+/*   Updated: 2025/04/27 16:13:30 by rkerman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	countwords(char *str)
 
 	c = 0;
 	in_w = 0;
-	while (*str)
+	while (str && *str)
 	{
 		if (!in_w && *str != ' ')
 		{
@@ -34,56 +34,118 @@ int	countwords(char *str)
 	return (c);
 }
 
-int	is_num(char *s)
+int	is_hex(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (s[i] == ',' || s[i] == ' '
-		|| s[i] == '\n' || (s[i] >= '0' && s[i] <= '9')
-		|| s[i] == '-' || (s[i] >= 'a' && s[i] <= 'f') || s[i] == 'x'
-		|| (s[i] >= 'A' && s[i] <= 'F') || s[i] == 'X')
+	while (s && (s[i] == ',' || s[i] == ' '
+			|| s[i] == '\n' || (s[i] >= '0' && s[i] <= '9')
+			|| s[i] == '-' || (s[i] >= 'a' && s[i] <= 'f') || s[i] == 'x'
+			|| (s[i] >= 'A' && s[i] <= 'F') || s[i] == 'X'))
 		i++;
 	if (s[i] || !i)
-	{
-		printf("%c", s[i]);
 		return (0);
-	}
 	return (1);
 }
 
-int	file_processing(char *path, int **grid)
+int	file_checker(char *path, t_val *data)
 {
-	int	fd;
+	int		fd;
 	char	*line;
-	int	size;
-	int	c;
+	int		size;
 
 	fd = open(path, O_RDONLY);
 	if (fd != -1)
 	{
 		line = get_next_line(fd);
+		if (!line)
+			return (0);
 		size = countwords(line);
+		data->width = size;
 		while (line)
 		{
-			if (!size)
-				size = countwords(line);
-			if (size != countwords(line) || !is_num(line))
+			if (size != countwords(line) || !is_hex(line))
 				return (free(line), 0);
-			c++;
+			data->height++;
 			free(line);
 			line = get_next_line(fd);
 		}
+		data->size = data->width * data->height;
 		close(fd);
 		return (1);
 	}
+	return (0);
+}
+/*
+void	grid_fill()
+
+void	file_processing(char *path, int **grid, t_val *data)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(path, O_RDONLY);
+	line = get_next_line(fd);
+	data->width = countwords(line);
+	while (line)
+	{
+		if(grid_fill())
+		{
+			free(line);
+			line = get_next_line(fd);
+			data->size += countwords(line);
+			data->height++;
+		}
+		else
+		{
+			free_grid(grid);
+			free(line);
+			break;
+		}
+	}
+	close(fd);
+}
+*/
+
+
+int	grid_fill(char *path, int **grid, t_val *data)
+{
+	int	fd;
+
+	fd = open(path, O_RDONLY);
+	*grid = ft_calloc(data->size + 1, sizeof(int));
+	if (!*grid)
+		return (0);
+	
+}
+
+int	file_processing(char *path, int **grid, t_val *data)
+{
+	if (!file_checker(path, data) || !grid_fill(path, grid, data))
+		return (0);
+	return (1);
+}
+
+void	init_data(t_val *data)
+{
+	data->size = 0;
+	data->width = 0;
+	data->height = 0;
 }
 
 int	main(int argc, char **argv)
 {
 	int	*g;
+	t_val data;
 
-	if ((argc - 1) == 1 && file_processing(argv[1], &g))
+	init_data(&data);
+	if ((argc - 1) == 1 && file_processing(argv[1], &g, &data))
 	{
+		printf("nombre d'element dans une ligne : %d\n", data.width);
+		printf("hauteur de la grille : %d\n", data.height);
+		printf("taille complete d'element : %d\n", data.size);
 	}
+	else
+		write(2, "Error map\n", 10);
 }
